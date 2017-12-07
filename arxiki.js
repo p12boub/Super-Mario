1cb091b4b1954518f661807ac7bd1c74dc4c9fa5
p12boub <mpoumpas93@gmail.com>
2017-12-07 16:09:06 +0200

    Update arxiki.js

--- arxiki.js
+++ arxiki.js
@@ -12,7 +12,7 @@ var arxiki= {
 		var background=game.add.sprite(0,0,'backg');
 		
 
-		var button1 = game.add.button(51 , 21, "level1");
+		var button1 = game.add.button(29, 48, "level1");
 		button1.anchor.set(0.5, 0.5);
 
 	}
