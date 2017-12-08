3feba475999e67696c11fcfc5762afc753480710
p12boub <mpoumpas93@gmail.com>
2017-12-08 13:30:32 +0200

    Add files via upload

---
+++ arxiki.js
@@ -0,0 +1,27 @@
+var arxiki = {
+preload: function() {
+    game.load.image('arxiki', 'assets/mariomenu.jpg');
+		game.load.image('Level1', 'assets/level1.png');
+		game.load.audio('supermariobros', 'audio/supermariobros.mp3');
+		},
+    
+    create: function() {
+		
+			supermariobros = game.add.audio('supermariobros');
+			supermariobros.play();
+
+			game.scale.pageAlignHorizontally = true;
+			game.scale.pageAlignVertically = true;
+			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+			game.physics.startSystem(Phaser.Physics.ARCADE);
+			
+			var menuPic = game.add.sprite(0, 0, 'arxiki');
+
+			
+			var click1 = game.add.button(110, 150, 'Level1', function() {
+			game.state.start('Level1'); 	
+			supermariobros.stop();
+		});
+		click1.anchor.set(0.5, 0.5);
+	}
+}
